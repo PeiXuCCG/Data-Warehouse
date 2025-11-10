e52000b5d85497f97202399ccf6ca523c805f336
chazabetz <chazabetz@gmail.com>
2025-11-10 13:06:07 +1100

    updating name of lakehouse to meet coding standards

---
+++ infrastructure/R2B_BC.Notebook/notebook-content.py
@@ -0,0 +1,139 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   }
+# META }
+
+# CELL ********************
+
+# %%
+from pyspark.sql import SparkSession
+from loom.tables.keyed_table import KeyedTable
+from loom.pipelines import Pipeline
+from pyspark.sql.functions import col, sha2
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# %%
+# These are the input variables for each bronze table
+source_table = "raw.customer"
+target_table = "customer"
+deduplicate_fields = ["customer_name", "phone", "address"] # please change this for each entity
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+def transform(df):
+    # NOT USED IN BC implementation
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# %%
+def deduplicate(df):
+
+    df = df.withColumn(
+        {target_table}_hk,
+        sha2(concat_ws("|", *[col(c) for c in deduplicate_fields]), 256)
+    )
+
+    return df
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# %%
+df = spark.read.table(source_table)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# %%
+# 4. Create the curated keyed table
+bronze_table =  table = KeyedTable(
+            target_db=target_db,
+            target_schema=target_schema,
+            name=target_table,
+            schema_evolution=False,
+            df=df,
+            target_path="", # NOT SUPPORTED IN FABRIC
+            business_keys=business_keys,
+            source_primary_keys=source_primary_keys,
+            source_foreign_keys=source_foreign_keys,
+            transform=transform, # YOU CAN CHANGE THIS TO NONE if there is not deduplication function
+            deduplicate=deduplicate # YOU CAN CHANGE THIS TO NONE if there is not deduplication function
+)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# %%
+# 5. Build the pipeline
+pipeline = Pipeline(
+    name=pipeline_name,
+    tables=bronze_table,
+    dry_run=True,   # simulate execution without writing
+)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# %%
+# 6. Execute the pipeline
+pipeline.summary()   # Prints info about each table
+pipeline.validate()  # Validates structure and metadata
+pipeline.execute()   # Runs the prepare + write steps
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
