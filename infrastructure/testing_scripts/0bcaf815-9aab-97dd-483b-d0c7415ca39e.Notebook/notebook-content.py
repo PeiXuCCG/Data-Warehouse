6624a520e4f0795a39a7d0ef390b869f1bd71129
chazabetz <chazabetz@gmail.com>
2025-11-25 22:00:16 +1100

    Committing 3 items from workspace 0380b4e3-57d1-4574-abb4-3f7e7e8427d0

--- infrastructure/testing_scripts/0bcaf815-9aab-97dd-483b-d0c7415ca39e.Notebook/notebook-content.py
+++ infrastructure/testing_scripts/0bcaf815-9aab-97dd-483b-d0c7415ca39e.Notebook/notebook-content.py
@@ -8,25 +8,26 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "059ddf57-7cf2-401b-8bf8-68beddef9667",
+# META       "default_lakehouse": "b66c596f-0807-41d8-b506-c2b21fcb9f29",
 # META       "default_lakehouse_name": "lh_bronze",
-# META       "default_lakehouse_workspace_id": "0380b4e3-57d1-4574-abb4-3f7e7e8427d0",
+# META       "default_lakehouse_workspace_id": "ac4f6d6f-0d6d-4c24-b56e-a49baf8c7706",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "059ddf57-7cf2-401b-8bf8-68beddef9667"
+# META           "id": "b66c596f-0807-41d8-b506-c2b21fcb9f29"
 # META         }
 # META       ]
 # META     },
 # META     "environment": {
-# META       "environmentId": "19ef04e9-33e6-8865-4282-9c499f72e816",
-# META       "workspaceId": "00000000-0000-0000-0000-000000000000"
+# META       "environmentId": "39f1badd-f616-482b-9dc2-e9db7c8b2617",
+# META       "workspaceId": "ac4f6d6f-0d6d-4c24-b56e-a49baf8c7706"
 # META     }
 # META   }
 # META }
 
 # CELL ********************
 
-# COMPARE THE ROW COUNTS ON THE BC OBJECTS WITH WHAT IS ON PAGE 8700 in BC
+# YOU NEED TO UPLOAD THE BC COUNTS FILE FROM PAGE 8700 and save it in the testing folder
+bc_count_file = "/lakehouse/default/Files/testing/Table Information.csv"
 
 # METADATA ********************
 
@@ -38,8 +39,10 @@
 # CELL ********************
 
 from pyspark.sql import Row
-from pyspark.sql.functions import col
+from pyspark.sql.functions import * 
 import pandas as pd
+import re
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 # METADATA ********************
 
@@ -77,16 +80,6 @@ company = [
   # ("MERITS","Netsuite"),
   # ("TCCG & HomeModifications", "Windward")
 ]
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
 
 # METADATA ********************
 
@@ -165,7 +158,7 @@ for name, system in company:   # your original "for name, system in company:"
 
             if spark.catalog.tableExists(table):
                 df = spark.sql(f"""
-                    SELECT COUNT(DISTINCT *) as cnt
+                    SELECT COUNT(*) as cnt
                     FROM {table}
                     WHERE company = '{name}'
                 """)
@@ -194,6 +187,85 @@ pivot_df = (
 
 display(pivot_df)
 
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+def cleanse(df):
+    pattern = r"[^a-z0-9_]+"
+
+    new_cols = {}
+    for col in df.columns:
+        cleaned = re.sub(pattern, "", col.split("-")[0].lower())
+        new_cols[col] = cleaned
+
+    df_cleaned = df.rename(columns=new_cols)
+
+    return df_cleaned
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Load data into pandas DataFrame from "/lakehouse/default/Files/testing/Table Information.csv"
+df = pd.read_csv("/lakehouse/default/Files/testing/Table Information.csv")
+rows = []
+for name, system in company:
+    filtered_df = df[df["Company Name"].str.lower() == name.lower()]
+    filtered_df = cleanse(filtered_df)
+    for e in entities:
+        # Get the row for that entity
+        row = filtered_df[filtered_df["tablename"].str.lower().str.replace(r'[^a-z0-9]', '', regex=True) == e.lower()]
+
+        value = row["noofrecords"].iloc[0] if not row.empty else None
+
+        rows.append({
+            "entity": e, 
+            "company": name,
+            "value": str(value) if value is not None else None
+        })
+
+
+schema = StructType([
+    StructField("entity", StringType(), True),
+    StructField("company", StringType(), True),
+    StructField("value", StringType(), True)   # Spark LongType == pandas int64
+])
+
+spark_df = spark.createDataFrame(rows, schema=schema)
+
+
+pivot_df2 = (
+    spark_df
+        .groupBy("entity")
+        .pivot("company")
+        .agg(first("value"))
+)
+
+# Rename to BC_<company>
+for name, system in company:
+    pivot_df2 = pivot_df2.withColumnRenamed(name, f"BC_{name}")
+
+
+columns_to_select = ["entity"]
+for name, system in company:
+    columns_to_select.append(f"`{name}`")
+    columns_to_select.append(f"`BC_{name}`")
+
+display(pivot_df2.join(pivot_df, "entity", "outer").select(columns_to_select))
 
 # METADATA ********************
 
