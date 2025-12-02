5dfbed5b00a268c5c64034a5cbca0df40664369b
AussieChas <charlesabetz@gmail.com>
2025-12-02 14:56:49 +1100

    fix pipeline for shortcutdimension1code

--- workflowgeneration/generate_pipeline.py
+++ workflowgeneration/generate_pipeline.py
@@ -98,15 +98,15 @@ if __name__ == "__main__":
     source_schema = "raw"
     target_lakehouse = "lh_bronze"
     target_schema = "bronze"
-    table_prefix = "historical" #bc
+    table_prefix = "bc" #bc
     path_to_excel = "../documentation/historical_bronze_workflow_mapping.xlsx"
     output_path = "pipeline.json"
-    notebook_id =  "91dad485-d600-8560-4fea-29a406f901ff"   
+    notebook_id =  "11363096-0db8-9724-4979-fb4b00909b73"   
     
     #"91dad485-d600-8560-4fea-29a406f901ff" - historical
     #"11363096-0db8-9724-4979-fb4b00909b73" - bc
 
-    output = build_pipeline_json(historical_job_configuration, job_name, source_lakehouse, source_schema, target_lakehouse, target_schema, table_prefix, notebook_id)
+    output = build_pipeline_json(bc_job_configuration, job_name, source_lakehouse, source_schema, target_lakehouse, target_schema, table_prefix, notebook_id)
 
     # write to disk
     with open(output_path, "w", encoding="utf-8") as f:
